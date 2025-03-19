d8f670b480078dfcb94e6dbfd0fd1df34449a032
vibinsgit <vibinsv810@gmail.com>
2025-03-19 23:27:12 +0530

    common uploaded

---
+++ common/dist/index.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.updateBlogInput = exports.createBlogInput = exports.signinInput = exports.signupInput = void 0;
+const zod_1 = __importDefault(require("zod"));
+//zod validation
+exports.signupInput = zod_1.default.object({
+    name: zod_1.default.string(),
+    username: zod_1.default.string().email(),
+    password: zod_1.default.string().min(8),
+});
+exports.signinInput = zod_1.default.object({
+    username: zod_1.default.string().email(),
+    password: zod_1.default.string().min(8),
+});
+exports.createBlogInput = zod_1.default.object({
+    title: zod_1.default.string(),
+    content: zod_1.default.string(),
+});
+exports.updateBlogInput = zod_1.default.object({
+    id: zod_1.default.number(),
+    title: zod_1.default.string(),
+    content: zod_1.default.string(),
+});
---
+++ common/src/index.ts
@@ -0,0 +1,30 @@
+import z from "zod";
+
+//zod validation
+export const signupInput = z.object({
+  name: z.string(),
+  username: z.string().email(),
+  password: z.string().min(8),
+});
+
+export const signinInput = z.object({
+    username: z.string().email(),
+    password: z.string().min(8),
+});
+
+export const createBlogInput = z.object({
+    title: z.string(),
+    content: z.string(),
+});
+
+export const updateBlogInput = z.object({
+  id: z.number(),
+  title: z.string(),
+  content: z.string(),
+});
+
+//type inference
+export type SignupInput = z.infer<typeof signupInput>
+export type SigninInput = z.infer<typeof signinInput>
+export type CreateBlogInput = z.infer<typeof createBlogInput>
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>
